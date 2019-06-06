ce4c10da3a9df2a966b8a21f1516cca35d237508
Christian Wunderlich <ch.wunderlich@yahoo.de>
2019-06-06 09:51:51 +0200

    add basic unit test setup

---
+++ test/BinaryDataReader.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using BinaryDataReader.Lib;
+
+namespace BinaryDataReader.Lib.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
